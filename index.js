d3a360c579dd01df5a91f6c69e3d9dd3dd98283e
florence <loracodes3@gmail.com>
2024-10-07 18:50:06 +0300

    final commit

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 const divide= (parameter1,parameter2)=>parameter1/parameter2
 console.log(divide(2000,100))
 
-let square = number => number
+let square = number => number*number
 
 const add =(num1, num2) => num1+num2
