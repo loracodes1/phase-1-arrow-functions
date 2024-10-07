952c4883133a7276532fbc57f643c003429dea2d
florence <loracodes3@gmail.com>
2024-10-07 18:46:11 +0300

    add arrow function

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-const divide= (a,b)=>a/b
+// divide function expression
+const divide= (parameter1,parameter2)=>parameter1/parameter2
 console.log(divide(2000,100))
+
+let square = number => number
+
+const add =(num1, num2) => num1+num2
